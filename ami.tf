5e0be1a4812dbd711b33a8c0dcf85b1aea88a244
Diego Castillo <diegotony1396@gmail.com>
2022-06-27 17:28:45 -0500

    fix: filter variable update

--- ami.tf
+++ ami.tf
@@ -2,9 +2,9 @@ data "aws_ami" "this" {
   most_recent = true
 
   dynamic "filter"{
-    for_each = var.filter
-    name = var.filter.name
-    values = var.filter.values
+    for_each = toset(var.filter)
+    name = each.value.name
+    values = each.value.values
   }
 
   owners = var.owners
