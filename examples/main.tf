5a6729790b35ad438f0e4008a65eab2ed4e2cbcf
Diego Castillo <constant1n396@Diegos-MacBook-Pro.local>
2023-01-15 23:51:12 -0500

    update provider

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,6 @@
 module "my_instance" {
     source = "../"
-    # "git::https://github.com/diegotony/aws_ec2_instance.git?ref=update-module"
+    # "gst"
     name = "my_awesome_instance_2"
     description = "a simple test"
     key_name = "terraform"
